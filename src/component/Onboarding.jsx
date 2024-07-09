88b3c4f7ba4904eb4bd582804ab9455c2afe2208
Alexanderkoum <mbappekoum@gmail.com>
2024-07-09 12:17:34 -0400

    Update Onboarding.jsx

--- src/component/Onboarding.jsx
+++ src/component/Onboarding.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "./Button";
-import { motion } from "framer-motion";
+import { motion } from "/framer-motion";
 
 const Onboarding = () => {
   return (
